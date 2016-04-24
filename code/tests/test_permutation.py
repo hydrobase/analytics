0447654bc756746085b65d99a831809b60139d74
juanshishido <juanshishido@cal.berkeley.edu>
2016-04-24 11:46:49 -0700

    permutation tests

---
+++ code/tests/test_permutation.py
@@ -0,0 +1,21 @@
+import unittest
+
+import numpy as np
+
+from code.permutation import pt
+
+
+class PermutationTest(unittest.TestCase):
+
+    def test_p_value_zero(self):
+        np.random.seed(42)
+        N, m = 50, 25
+        t = np.random.normal(loc=10, size=m)
+        c = np.random.normal(loc=5, size=N-m)
+        arr = np.append(t, c)
+        self.assertEquals(0.0, pt(arr, m))
+
+    def test_p_value_one(self):
+        N, m = 10, 5
+        arr = np.ones(N)
+        self.assertEquals(1.0, pt(arr, m))
